7d65091728557eb8e8cfa60a6dfe72ce3b90efc7
Kaylene <viola.sox@gmail.com>
2018-09-30 23:05:08 -0400

    Working integral for HW2

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -1,45 +1,54 @@
 #include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "HW2.h"
 
 int main(int argc, char** argv) {
     int numP;
     int myRank;
     double startVal = 0;
     double endVal = 1;
-    int n = 10;
-    double deltaX = (startVal - endVal) / (double) n;
+    int n = 100;
+    double deltaX = (endVal - startVal) / (double) n;
 
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &numP);
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
     if (n % numP != 0) {
-        printf("Please make sure n is evenly divisible by p\n");
-        return(1);
+        if (myRank == 0)
+            printf("Please make sure n is evenly divisible by p\n");
+        MPI_Finalize();
+        return(0);
     }
 
     int pointsPerNode = n / numP;
     double curPoint = startVal + myRank*pointsPerNode*deltaX;
     double myIntegral = 0;
     for (int i = 0; i < pointsPerNode; i++) {
-        myIntegral += calcStep(curPoint, deltaX);
+        myIntegral += calcStep(curPoint, deltaX, myRank);
         curPoint += deltaX;
     }
+
     if (myRank != 0)
         MPI_Send(&myIntegral, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     else {
         double data;
         for (int i = 1; i < numP; i++) {
-            MPI_RECV(&data, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&data, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             myIntegral += data;
         }
         printf("The integral is %f\n", myIntegral);
     }
+
+    MPI_Finalize();
     return(0);
 }
 
 // Calulate one discrete step of the integral
-double calcStep(double x, double deltaX) {
-    double numerator = function(x) + function(x - deltaX);
+double calcStep(double x, double deltaX, int myRank) {
+    double numerator = function(x) + function(x + deltaX);
+    printf("Process %d: Integral from %f to %f is %f\n", myRank, x, x+deltaX, (numerator*deltaX/2));
     return((numerator / 2) * deltaX);
 }
 
---
+++ HW2/HW2.h
@@ -0,0 +1,2 @@
+double calcStep(double x, double deltaX, int myRank);
+double function(double x);
