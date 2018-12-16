94ce62516c768e3ccd4074a5170be7fe0c6d9365
Kaylene <violasox@gmail.com>
2018-12-16 17:14:50 -0500

    Removed old functions from header

--- Project4/solver.h
+++ Project4/solver.h
@@ -1,5 +1,3 @@
-void updateMeshJacobi(double* mesh, double* newMesh, int xSize, int ySize, double xStep, double yStep, bool verifyMode);
-double sourceTerm(double x, double y, bool verifyMode);
 void printMesh(double* mesh, int xSize, int ySize);
 void setBounds(double* mesh, int xSize, int ySize, double xStep, double yStep, bool verifyMode);
 void copyMesh(double* mesh, double* newMesh, int numElements);
